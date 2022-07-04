d7a4e33d25ca0f63d229a1ebc9b450a610544923
Marina Nadj <marinanadj7@yahoo.com>
2022-07-04 18:57:44 +0200

    new

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
-import React, { Component } from 'react'
+import React, { useState, createContext, useContext, useEffect } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './config/firebase';
 
 // import the screens
 import Home from './components/Home';
@@ -14,24 +18,60 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Create the navigator
 const Stack = createStackNavigator();
 
-export default class App extends React.Component{
+// Create a contect for the user data
+const AuthenticatedUserContext = createContext({});
 
-  render() {
+// Create a Provider function to allow screen components to acces the current user
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
+
+function RootNavigator() {
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // onAuthStateChanges returns an unsubscriber
+    const unsubscribeAuth = onAuthStateChanged(
+      auth,
+      async authenticatedUser => {
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+        setIsLoading(false);
+      }
+    );
+
+    // unsubscribe auth listener to unmount
+    return unsubscribeAuth;
+  }, [user]);
+
+  if (isLoading) {
     return (
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-        >
-          <Stack.Screen
-            name="Chat App"
-            component={Home}
-          />
-          <Stack.Screen
-            name="Chat"
-            component={Chat}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
     );
   }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Chat' component={Chat} />
+        <Stack.Screen name='Start' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
+  );
 }
