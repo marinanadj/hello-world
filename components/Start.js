386a23e4cbf787a7b510b8a73ed38ddab875cd24
Marina Nadj <marinanadj7@yahoo.com>
2022-07-09 14:30:02 +0200

    up

--- components/Start.js
+++ components/Start.js
@@ -19,33 +19,6 @@ export default function Start(props) {
   // State to hold information if user is offline or online
   const [isConnected, setIsConnected] = useState(false);
 
-  // Authenticate the user via Firebase and then redirect to the chat screen, passing the name and color props
-  const onHandleStart = () => {
-    if (isConnected) {
-      signInAnonymously(auth)
-        .then(() => {
-          console.log('Login success');
-          props.navigation.navigate('Chat', { name: name, color: color });
-        })
-        .catch(err => console.log(`Login err: ${err}`));
-    }
-    else {
-      props.navigation.navigate('Chat', { name: name, color: color });
-    }
-  }
-
-  useEffect(() => {
-
-    // Check if user is offline or online using NetInfo
-    NetInfo.fetch().then(connection => {
-      if (connection.isConnected) {
-        setIsConnected(true);
-      } else {
-        setIsConnected(false);
-      }
-    });
-
-  })
 
 
   return (
