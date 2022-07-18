9e358ccaec8e76cf1d87428adfbb95ad6b0fd1ea
Marina Nadj <marinanadj7@yahoo.com>
2022-07-18 13:23:09 +0200

    lkm

--- components/Start.js
+++ components/Start.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextInput, View, Pressable, TouchableOpacity, ImageBackground, Platform, KeyboardAvoidingView } from 'react-native';
 import BackgroundImage from '../img/BackgroundImage.png';
 
 import { signInAnonymously } from "firebase/auth";
 import { auth } from '../config/firebase';
 
+import NetInfo from '@react-native-community/netinfo';
+
 // Create constant that holds background colors for Chat Screen
 const colors = {
-    black: "#090C08",
+    pink: "#FFC0CB",
     purple: "#474056",
     grey: "#8A95A5",
     green: "#B9C6AE",
@@ -17,15 +19,36 @@ export default function Start(props) {
     let [name, setName] = useState();
     let [color, setColor] = useState();
 
+    // State to hold information if user is offline or online
+    const [isConnected, setIsConnected] = useState(false);
+
     // Authenticate the user via Firebase and then redirect to the chat screen, passing the name and color props
     const onHandleStart = () => {
-        signInAnonymously(auth)
-            .then(() => {
-                console.log('Login success');
-                props.navigation.navigate('Chat', { name: name, color: color });
-            })
-            .catch(err => console.log(`Login err: ${err}`));
+        if (isConnected) {
+            signInAnonymously(auth)
+                .then(() => {
+                    console.log('Login success');
+                    props.navigation.navigate('Chat', { name: name, color: color });
+                })
+                .catch(err => console.log(`Login err: ${err}`));
+        }
+        else {
+            props.navigation.navigate('Chat', { name: name, color: color });
+        }
     }
+
+    useEffect(() => {
+
+        // Check if user is offline or online using NetInfo
+        NetInfo.fetch().then(connection => {
+            if (connection.isConnected) {
+                setIsConnected(true);
+            } else {
+                setIsConnected(false);
+            }
+        });
+
+    })
 
 
     return (
@@ -36,7 +59,7 @@ export default function Start(props) {
                 style={styles.image}
             >
 
-                <Text style={styles.title}>ChatApp</Text>
+                <Text style={styles.title}>Chat App</Text>
 
                 <View style={styles.box}>
 
@@ -52,8 +75,8 @@ export default function Start(props) {
                     <Text style={styles.text}>Choose Background Color:</Text>
                     <View style={styles.colorContainer}>
                         <TouchableOpacity
-                            style={[{ backgroundColor: colors.black }, styles.colorbutton]}
-                            onPress={() => setColor(colors.black)}
+                            style={[{ backgroundColor: colors.pink }, styles.colorbutton]}
+                            onPress={() => setColor(colors.pink)}
                         />
                         <TouchableOpacity
                             style={[{ backgroundColor: colors.purple }, styles.colorbutton]}
