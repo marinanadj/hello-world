33650b072a2ea41cd24db87037358c0bf3c20064
Marina Nadj <marinanadj7@yahoo.com>
2022-07-20 14:14:01 +0200

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,16 @@
+import React, { useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDRpnQNgXcRpr1TDBFhmA2eP-2D7ANVJ_o",
-  authDomain: "web-app-5e13f.firebaseapp.com",
-  projectId: "web-app-5e13f",
-  storageBucket: "web-app-5e13f.appspot.com",
-  messagingSenderId: "1059875359492",
-  appId: "1:1059875359492:web:993c18589fb6ef151b9e51",
-  measurementId: "G-S526CTD1E2"
+apiKey: "AIzaSyDRpnQNgXcRpr1TDBFhmA2eP-2D7ANVJ_o",
+authDomain: "web-app-5e13f.firebaseapp.com",
+projectId: "web-app-5e13f",
+storageBucket: "web-app-5e13f.appspot.com",
+messagingSenderId: "1059875359492",
+appId: "1:1059875359492:web:993c18589fb6ef151b9e51",
+measurementId: "G-S526CTD1E2"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
