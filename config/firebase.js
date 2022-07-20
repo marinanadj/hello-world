64b2a4644852b4300b1e682b417c1bedce79b9b4
Marina Nadj <marinanadj7@yahoo.com>
2022-07-20 14:12:10 +0200

    firebase update

--- config/firebase.js
+++ config/firebase.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Firebase config
 const firebaseConfig = {
@@ -12,26 +13,8 @@ const firebaseConfig = {
 };
 
 
-// import react native gesture handler
-import "react-native-gesture-handler";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// import react Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-import Start from "../components/Start";
-import Chat from "../components/Chat";
-
-// Create the navigator
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Screen1">
-        <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Chat" component={Chat} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+// Initialize Cloud Firestore and get a reference to the service (db)
+export const db = getFirestore(app);
